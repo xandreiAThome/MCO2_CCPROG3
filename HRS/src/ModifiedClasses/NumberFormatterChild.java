040bcfbffd3b6ce0c348c4f37750353523841984
Ellexandrei Esponilla <xandrei37@gmail.com>
2024-07-22 22:21:11 +0800

    forgot to add files

---
+++ HRS/src/ModifiedClasses/NumberFormatterChild.java
@@ -0,0 +1,21 @@
+package ModifiedClasses;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import javax.swing.text.NumberFormatter;
+
+public class NumberFormatterChild extends NumberFormatter {
+    public NumberFormatterChild(NumberFormat format) {
+        super(format);
+    }
+
+    @Override
+    public Object stringToValue(String text) throws ParseException {
+        if (text.length() == 0) {
+            return null;
+        }
+        // TODO Auto-generated method stub
+        return super.stringToValue(text);
+    }
+}
